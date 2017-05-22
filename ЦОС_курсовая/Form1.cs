1bdbc33694f91a8b0afc1ffc8eed83babbd60328
d3m1d0v <y.a.demidov@yandex.ru>
2017-05-22 18:12:45 +0300

    статические переменные

--- ЦОС_курсовая/Form1.cs
+++ ЦОС_курсовая/Form1.cs
@@ -19,43 +19,44 @@ namespace ЦОС_курсовая
         //=====================//
         // Вариант 1
         //=====================//
-        //int f_p = 5; // Гц
-        //int f_a = 8; // Гц
-        //int A_p = 1;  // дБ
-        //int A_a = 30; // дБ 
+        //static int f_p = 5; // Гц
+        //static int f_a = 8; // Гц
+        //static int A_p = 1;  // дБ
+        //static int A_a = 30; // дБ 
         //=====================//
-        //int f1 = 1;  // Гц
-        //int f2 = 5; // Гц
-        //int f3 = 7; // Гц
-        //int f4 = 8; // Гц
-        //int f5 = 30; // Гц
+        //static int f1 = 1;  // Гц
+        //static int f2 = 5; // Гц
+        //static int f3 = 7; // Гц
+        //static int f4 = 8; // Гц
+        //static int f5 = 30; // Гц
         //=====================//
         // Вариант 5
         //=====================//
-        int f_p = 25; // Гц
-        int f_a = 40; // Гц
-        int A_p = 1;  // дБ
-        int A_a = 30; // дБ
+        static int f_p = 25; // Гц
+        static int f_a = 40; // Гц
+        static int A_p = 1;  // дБ
+        static int A_a = 30; // дБ
         //=====================//
-        int f1 = 1;  // Гц
-        int f2 = 25; // Гц
-        int f3 = 30; // Гц
-        int f4 = 40; // Гц
-        int f5 = 60; // Гц
+        static int f1 = 1;  // Гц
+        static int f2 = 25; // Гц
+        static int f3 = 30; // Гц
+        static int f4 = 40; // Гц
+        static int f5 = 60; // Гц
         //=====================//
         //=====================//
         // Вариант 13
         //=====================//
-        //int f_p = 8; // Гц
-        //int f_a = 12; // Гц
-        //int A_p = 2;  // дБ
-        //int A_a = 30; // дБ
+        //static int f_p = 8; // Гц
+        //static int f_a = 12; // Гц
+        //static int A_p = 2;  // дБ
+        //static int A_a = 30; // дБ
         //=====================//
-        //int f1 = 1;  // Гц
-        //int f2 = 8; // Гц
-        //int f3 = 10; // Гц
-        //int f4 = 12; // Гц
-        //int f5 = 50; // Гц
+        //static int f1 = 1;  // Гц
+        //static int f2 = 8; // Гц
+        //static int f3 = 10; // Гц
+        //static int f4 = 12; // Гц
+        //static int f5 = 50; // Гц
         //=====================//
+
     }
 }
